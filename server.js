95f5ab400664c55ad6fe404fb4e280a8f1a77558
HansithFonseka <hansithlochana@gmail.com>
2024-07-13 17:59:09 +0530

    api

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDB = require('./db');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const app = express();
+const port = 3001;
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Routes
+app.use('/api/depression', require('./routes/depression'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/login', require('./routes/login'));
+
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
